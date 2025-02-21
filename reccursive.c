ff224ae19c51d4fa0b283f2c74a05ad93f057a87
cyanolupus <cyanolupus@gmail.com>
2025-02-21 13:29:48 +0900

    format

--- reccursive.c
+++ reccursive.c
@@ -5,121 +5,163 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {
-    TK_RESERVED,
-    TK_NUM,
-    TK_EOF,
+typedef enum
+{
+  TK_RESERVED,
+  TK_NUM,
+  TK_EOF,
 } TokenKind;
 
-void error(char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
+char* user_input;
+
+void
+error(char* fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+void
+error_at(char* loc, char* fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
 }
 
 typedef struct Token Token;
 
-struct Token {
-    TokenKind kind;
-    Token *next;
-    int val;
-    char *str;
+struct Token
+{
+  TokenKind kind;
+  Token* next;
+  int val;
+  char* str;
 };
 
-bool consume(Token** self, char op) {
-    if ((*self)->kind != TK_RESERVED || (*self)->str[0] != op)
-        return false;
-    *self = (*self)->next;
-    return true;
+bool
+consume(Token** self, char op)
+{
+  if ((*self)->kind != TK_RESERVED || (*self)->str[0] != op)
+    return false;
+  *self = (*self)->next;
+  return true;
 }
 
-void expect(Token** self, char op) {
-    if ((*self)->kind != TK_RESERVED || (*self)->str[0] != op)
-        error("Token is not '%c', but '%c'", op, (*self)->str[0]);
-    *self = (*self)->next;
+void
+expect(Token** self, char op)
+{
+  if ((*self)->kind != TK_RESERVED || (*self)->str[0] != op)
+    error_at((*self)->str, "Token is not '%c', but '%c'", op, (*self)->str[0]);
+  *self = (*self)->next;
 }
 
-int expect_number(Token **self) {
-    if ((*self)->kind != TK_NUM)
-        error("Token is not a number");
-    int val = (*self)->val;
-    *self = (*self)->next;
-    return val;
+int
+expect_number(Token** self)
+{
+  if ((*self)->kind != TK_NUM)
+    error_at((*self)->str, "Token is not number");
+  int val = (*self)->val;
+  *self = (*self)->next;
+  return val;
 }
 
-bool at_eof(Token *self) {
-    return self->kind == TK_EOF;
+bool
+at_eof(Token* self)
+{
+  return self->kind == TK_EOF;
 }
 
-void view_token(Token *self) {
-    while (self->kind != TK_EOF) {
-        printf("kind: %d, val: %d, str: %s, char: %c\n", self->kind, self->val, self->str, self->str[0]);
-        self = self->next;
-    }
+void
+view_token(Token* self)
+{
+  while (self->kind != TK_EOF) {
+    printf("kind: %d, val: %d, str: %s, char: %c\n",
+           self->kind,
+           self->val,
+           self->str,
+           self->str[0]);
+    self = self->next;
+  }
 }
 
-Token *new_token(TokenKind kind, Token *cur, char *str) {
-  Token *tok = calloc(1, sizeof(Token));
+Token*
+new_token(TokenKind kind, Token* cur, char* str)
+{
+  Token* tok = calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->str = str;
   cur->next = tok;
   return tok;
 }
 
-Token *tokenize(char *p) {
-    Token head;
-    head.next = NULL;
-    Token *cur = &head;
+Token*
+tokenize(char* p)
+{
+  Token head;
+  head.next = NULL;
+  Token* cur = &head;
 
-    while (*p) {
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if (*p == '+' || *p == '-') {
-            cur = new_token(TK_RESERVED, cur, p++);
-            continue;
-        }
-
-        if (isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p);
-            cur->val = strtol(p, &p, 10);
-            continue;
-        }
-
-        error("cant tokenize");
+  while (*p) {
+    if (isspace(*p)) {
+      p++;
+      continue;
     }
 
-    new_token(TK_EOF, cur, p);
-    return head.next;
+    if (*p == '+' || *p == '-') {
+      cur = new_token(TK_RESERVED, cur, p++);
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p);
+      cur->val = strtol(p, &p, 10);
+      continue;
+    }
+
+    error_at(p, "cant tokenize");
+  }
+
+  new_token(TK_EOF, cur, p);
+  return head.next;
 }
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        printf("Usage: %s <expr>\n", argv[0]);
-        return 1;
+int
+main(int argc, char** argv)
+{
+  if (argc != 2) {
+    printf("Usage: %s <expr>\n", argv[0]);
+    return 1;
+  }
+
+  Token* token;
+  token = tokenize(argv[1]);
+  user_input = argv[1];
+
+  printf(".global _main\n");
+  printf("_main:\n");
+  printf("  mov x0, %d\n", expect_number(&token));
+
+  while (!at_eof(token)) {
+    if (consume(&token, '+')) {
+      printf("  add x0, x0, %d\n", expect_number(&token));
+      continue;
     }
 
-    Token *token;
-    token = tokenize(argv[1]);
+    expect(&token, '-');
+    printf("  sub x0, x0, %d\n", expect_number(&token));
+  }
 
-    printf(".global _main\n");
-    printf("_main:\n");
-    printf("  mov x0, %d\n", expect_number(&token));
-
-    while (!at_eof(token)) {
-        if (consume(&token, '+')) {
-            printf("  add x0, x0, %d\n", expect_number(&token));
-            continue;
-        }
-
-        expect(&token, '-');
-        printf("  sub x0, x0, %d\n", expect_number(&token));
-    }
-
-    printf("  ret\n");
-    return 0;
+  printf("  ret\n");
+  return 0;
 }
